6c85c0fca958566c898d18925b9fe5785589196a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-19 19:37:47 +0000

    Deploy factset/quart-openapi to github.com/factset/quart-openapi.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.6',
+    URL_ROOT: '',
+    VERSION: '1.3.7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
