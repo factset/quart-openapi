98e2ffebeedd1506af791950931be8d5910a6267
Hutson Betts <hbetts@factset.com>
2019-02-04 12:09:40 -0600

    chore: bump version to 1.4.1
    Bump version to test new publishing strategy.

--- quart_openapi/__about__.py
+++ quart_openapi/__about__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 __short_version__ = '1.4'
-__release__ = '1.4.0'
+__release__ = '1.4.1'
 __description__ = 'Framework for Quart to add swagger generation to routes and restful resources'
