c6ae2b75682bd94caa1cf7d9595753932b0d5f91
Matthew Topol <mtopol@factset.com>
2020-02-10 14:20:46 -0500

    chore(version): bump to 1.5.2

--- quart_openapi/__about__.py
+++ quart_openapi/__about__.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
 # -*- coding: utf-8 -*-
 __short_version__ = '1.5'
-__release__ = '1.5.1'
+__release__ = '1.5.2'
 __description__ = 'Framework for Quart to add swagger generation to routes and restful resources'
